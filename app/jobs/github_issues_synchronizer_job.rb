6b9976c138079342c0176d09715037fbbb619f51
Mohamed Nabil <mohamednabil00000@gmail.com>
2026-03-01 15:25:57 +0200

    Created cron task to get the new issues regularly. (#21)

---
+++ app/jobs/github_issues_synchronizer_job.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class GithubIssuesSynchronizerJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    last_issue_id = $redis.get("last_issue_id")
+    offset_reached = false
+    page = 1
+    first_issue_id = nil
+
+    while !offset_reached
+      result = Github::RailsRepo::Client.get_issues(page:)
+      return unless result.success?
+
+      parsed_data_result = GithubRepoData::ParsingService.call(data: result.payload, offset: last_issue_id)
+      return unless parsed_data_result.success?
+
+      break if parsed_data_result.payload[:issues].empty?
+
+      if first_issue_id.nil?
+        first_issue_id = parsed_data_result.payload[:issues][0][:id]
+      end
+
+      offset_reached = parsed_data_result.payload[:offset_reached]
+
+      persisted_data_result = GithubRepoData::PersistingService.call(
+        users_data: parsed_data_result.payload[:users],
+        issues_data: parsed_data_result.payload[:issues]
+      )
+      return unless persisted_data_result.success?
+
+      page += 1
+    end
+    $redis.set("last_issue_id", first_issue_id) if first_issue_id.present?
+  end
+end
